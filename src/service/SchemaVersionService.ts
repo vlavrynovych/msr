38077eb59b3e7d627a3d01bfa5561854b7ebb825
Volodymyr Lavrynovych <volodyalavrynovych@gmail.com>
2023-11-06 10:40:30 +0200

    #7: Implementing it in more declarative way with more interfaces

--- src/service/SchemaVersionService.ts
+++ src/service/SchemaVersionService.ts
@@ -1,25 +1,24 @@
 import {MigrationScript} from "../model";
-import {IRunner, IMigrationInfo} from "../interface";
+import {IMigrationInfo, IMigrationScript, ISchemaVersion, ISchemaVersionService} from "../interface";
 
-export class SchemaVersionService {
-    constructor(private runner: IRunner) {}
+export class SchemaVersionService<T extends ISchemaVersion & IMigrationScript> implements ISchemaVersionService{
+    constructor(private service: T) {}
 
-    public async init():Promise<void> {
-        const table = this.runner.cfg.tableName;
-        const init = await this.runner.isInitialized(table);
+    public async init(tableName:string):Promise<void> {
+        const init = await this.service.isInitialized(tableName);
         if(!init) {
-            const created = await this.runner.createTable(table);
+            const created = await this.service.createTable(tableName);
             if(!created) throw new Error("Cannot create table")
         }
-        const isValid = await this.runner.validateTable(table);
+        const isValid = await this.service.validateTable(tableName);
         if(!isValid) throw new Error("Schema version table is invalid")
     }
 
     public async register(details:IMigrationInfo):Promise<any> {
-        return this.runner.register(details);
+        return this.service.register(details);
     }
 
     public async getAllMigratedScripts():Promise<MigrationScript[]> {
-        return await this.runner.getAll();
+        return await this.service.getAll();
     }
 }
